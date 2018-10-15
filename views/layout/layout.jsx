2dadb8f68c4b91a78aa940db0c1fec400de77a96
chrismintan <chris_acs@hotmail.com>
2018-10-15 17:59:43 +0800

    changed name

--- views/layout/layout.jsx
+++ views/layout/layout.jsx
@@ -85,7 +85,7 @@ class Layout extends React.Component {
         <body>
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
           <div className="container">
-            <a className="navbar-brand mb-0 h1" href="/">Onion or Not!</a>
+            <a className="navbar-brand mb-0 h1" href="/">Spot the Fake!</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
